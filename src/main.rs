40a76a48bd52817129e4276d8543d5cebbcb95d5
heartleth <myjmyjmyj237@gmail.com>
2023-10-06 14:57:08 +0900

    commans lines

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,50 @@ pub mod assembling;
 pub mod examples;
 pub mod lan;
 
-use lan::Parser;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// path of file to parse.
+    #[arg(short = 't', long)]
+    doc: String,
+    
+    /// Lan grammer paths, separated by ",".
+    #[arg(short, long)]
+    spec: String,
+    
+    /// Lan dictionary paths, separated by ",".
+    #[arg(short, long)]
+    dict: String
+}
 
 fn main() {
-    // examples::english();
-    let parser_english = Parser::open("examples/lans_japanese/main.lan", "examples/dictionary/dictionary.dic").unwrap();
+    let args = Args::parse();
+    let specs :Vec<&str> = args.spec.split(',').collect();
+    let dicts :Vec<&str> = args.dict.split(',').collect();
+    
+    println!("{:?}", specs);
+    println!("{:?}", dicts);
+    let mut lanparser = lan::Parser::open(specs[0], dicts[0]).unwrap();
+    let mut i = 0;
+    for dictf in dicts {
+        if i > 0 {
+            lanparser.load_dict(dictf).unwrap();
+        }
+        i += 1;
+    }
+    i = 0;
+    for specf in specs {
+        if i > 0 {
+            lanparser.load_lan(specf);
+        }
+        i += 1;
+    }
+    
 
-    parser_english.with_parser(|p| {
-        let t = std::fs::read_to_string("examples/sentences_japanese.txt").unwrap();
+    lanparser.with_parser(|p| {
+        let t = std::fs::read_to_string(&args.doc).unwrap();
         for text in t.split("\n") {
             if text.starts_with('#') {
                 println!("{}", &text[2..]);
@@ -28,7 +64,7 @@ fn main() {
                 }
             }
             else {
-                println!("INVALID");
+                println!("INVALID\n");
             }
         }
         Some(())
